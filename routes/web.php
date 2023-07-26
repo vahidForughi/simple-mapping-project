2d69b0aefb561baf90fcb03afcb42a71fc08078d
vahid forughi <forughi.vahid@yahoo.com>
2023-07-26 18:11:48 +0330

    add rate limit for routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth.basic'])->group(function () {
+Route::middleware(['auth.basic', 'throttle:10,1'])->group(function () {
 
     Route::get('/teams/sync/xml', [TeamController::class, 'xmlSync']);
     Route::get('/teams/sync', [TeamController::class, 'sync']);
